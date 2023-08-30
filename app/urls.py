cd31e3e36a0b153c0532236ec31b80235bca493e
clmagno <chris.ultd@gmail.com>
2023-08-30 16:01:42 +0800

    initial upload

---
+++ app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('',views.index, name='index'),
+    path('members/',views.home, name='members'),
+    path('members/add/', views.add, name='add'),
+    path('members/add/addrecord/', views.addrecord, name='addrecord'),
+    path('members/update/<int:id>', views.update, name='update'),
+    path('members/update/updaterecord/<int:id>', views.updaterecord, name='updaterecord'),
+    path('members/confirm_delete/<int:id>', views.confirm_delete, name='confirm_delete'),
+    path('members/confirm_delete/delete/<int:id>', views.delete, name='delete'),
+
+]
---
+++ app/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Member
+from django.template import loader
+from django.urls import reverse
+
+# Create your views here.
+def index(request):
+    return HttpResponse(loader.get_template('misc/index.html').render({},request))
+
+def home(request):
+    members = Member.objects.all().values()
+    template = loader.get_template('members/home.html')
+    context={
+        'members':members
+    }
+    return HttpResponse(template.render(context, request))
+
+def add(request):
+    template  = loader.get_template('members/add.html')
+    return HttpResponse(template.render({},request)) 
+
+def addrecord(request):
+    fn = request.POST['first']
+    ln = request.POST['last']
+    
+    member = Member(fname=fn, lname=ln)
+    member.save()
+    return HttpResponseRedirect(reverse('members'))
+
+def update(request,id):
+    member = Member.objects.get(id=id) #Select * from Member where id= id
+    
+    template = loader.get_template("members/update.html")
+    context={
+        'member':member
+    }
+    return HttpResponse(template.render(context,request))
+
+def updaterecord(request,id):
+    fn = request.POST['first']
+    ln = request.POST['last']
+    member = Member.objects.get(id=id)
+    member.fname = fn
+    member.lname = ln
+    member.save()
+    return HttpResponseRedirect(reverse('members'))
+
+def confirm_delete(request,id):
+    member = Member.objects.get(id=id)
+    template = loader.get_template("members/delete.html")
+    context ={
+        "member":member
+    }
+    return HttpResponse(template.render(context,request))
+    
+def delete(request,id):
+    member = Member.objects.get(id=id)
+    member.delete()
+    return HttpResponseRedirect(reverse('members'))
+
+
+
